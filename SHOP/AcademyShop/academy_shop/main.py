0c20af6ec389a94a543a06851fcf43173ba46676
amirali sahraei <a.sahraei98@gmail.com>
2022-04-13 16:41:33 +0430

    improve password generator

--- SHOP/AcademyShop/academy_shop/main.py
+++ SHOP/AcademyShop/academy_shop/main.py
@@ -1,6 +1,8 @@
+from cgi import print_arguments
 import string
 from random import choices
-
+import sys
+import argparse
 
 def create_password(length=8, upper=False, lower=False, digit=False, pun=False):
     
@@ -24,8 +26,20 @@ def create_password(length=8, upper=False, lower=False, digit=False, pun=False):
     return ''.join(choices(pool, k=length))
 
 if __name__ == "__main__":
-    print(create_password())
-    print(create_password(upper=True))
-    print(create_password(lower=True))
-    print(create_password(upper=True, lower=True, digit=True))
-    print(create_password(upper=True, digit=True, pun=True))
+    # try:
+    #     length = int(sys.argv[1])
+    # except ValueError:
+    #     print("please enter int")
+    # else:
+    #     print(create_password(length))
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('length', type=int, help="length of password")
+    parser.add_argument('-u', '--upper', help="User Upper case", action='store_true')
+    parser.add_argument('-l', '--lower', help="User lower case", action='store_true')
+    parser.add_argument('-d', '--digit', help="User digit case", action='store_true')
+    parser.add_argument('-p', '--pun', help="User punc case", action='store_true')
+
+    args = parser.parse_args()
+    print(create_password(args.length, args.upper, args.lower, args.digit, args.pun))
+
